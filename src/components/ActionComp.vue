ac85d531a7101870720852d2bd92a5bd47b6478e
PietroMarilei~ <pietromrl0@gmail.com>
2023-07-25 11:55:32 +0200

    done company sect

---
+++ src/components/ActionComp.vue
@@ -0,0 +1,78 @@
+<script>
+import { store } from '../store';
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<template>
+    <!-- footer template -->
+    <div class="primaryWhiteBg">
+        <div class="container">
+            <div class="row">
+                <p>
+                    WE DO MORE FOR EVERYONE
+                </p>
+
+                <h1>Actions & <span>Projects</span></h1>
+
+                <ul>
+                    <li>
+                        ALL
+                    </li>
+                    <li>
+                        INSTITUTIONAL
+                    </li>
+                    <li>
+                        SOCIAL
+                    </li>
+                    <li>
+                        EVENTS
+                    </li>
+                    <li>
+                        INNOVATIONS
+                    </li>
+                    <li>
+                        ENVIROMENTS
+                    </li>
+                    <li>
+                        TECHNOLOGY
+                    </li>
+                </ul>
+
+                <div class="card_wrapper">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+.row {
+    text-align: center;
+}
+
+ul {
+    display: flex;
+    justify-content: center;
+
+    padding: 2rem 0;
+
+    li {
+        padding: 0 1rem;
+    }
+}
+</style>
